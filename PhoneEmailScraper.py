733c0b20f4f78a5f1a4376fbfab3f56a1e59ace9
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-08 19:05:50 +0530

    initial commit

---
+++ PhoneEmailScraper.py
@@ -0,0 +1,22 @@
+import re
+
+import pyperclip
+
+# TODO: Create a regex for phone numbers
+re.compile(
+    r"""
+
+
+
+
+""",
+    re.VERBOSE,
+)
+
+# TODO: Create a regex for email addresses
+
+# TODO: Get the text off the clipboard
+
+# TODO: Extract the email/phone from this text
+
+# TODO: Copy the extracted email/phone to the clipboard
