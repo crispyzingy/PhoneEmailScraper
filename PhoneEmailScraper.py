ab79a72ec3cb9076b46285a542b6ff319cbdfd68
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-08 22:41:50 +0530

    fixed bugs

--- PhoneEmailScraper.py
+++ PhoneEmailScraper.py
@@ -7,13 +7,15 @@ phoneRegex = re.compile(
     r"""
 # 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
 
-(\d\d\d)|(\(\d\d\d\))?   # area code(optional)
+(
+((\d\d\d)|(\(\d\d\d\)))?   # area code(optional)
 (\s|-)                   # separator
 \d\d\d                   # first 3 digits
 -                        # separator
 \d\d\d\d                 # last 4 digits
 (((ext(\.)?\s)|x)        # extension word-part(optional)
 (\d{2,5}))?              # extension number-part(optional)
+)
 """,
     re.VERBOSE,
 )
@@ -26,7 +28,8 @@ emailRegex = re.compile(
 [a-zA-Z0-9_.+]+        # name part
 @                      # @symbol
 [a-zA-Z0-9_.+]+        # domain name part
-"""
+""",
+    re.VERBOSE,
 )
 
 # TODO: Get the text off the clipboard
@@ -36,5 +39,7 @@ text = pyperclip.paste()
 extractedPhone = phoneRegex.findall(text)
 extractedEmail = emailRegex.findall(text)
 
+print(extractedPhone)
+print(extractedEmail)
 
 # TODO: Copy the extracted email/phone to the clipboard
