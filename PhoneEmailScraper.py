7f93d58872661af6047a8ccbd80aa69c37f911ad
Arbaaz Khan <crispyzingy@gmail.com>
2020-03-08 22:19:21 +0530

    created regex for phone numbers

--- PhoneEmailScraper.py
+++ PhoneEmailScraper.py
@@ -2,13 +2,18 @@ import re
 
 import pyperclip
 
-# TODO: Create a regex for phone numbers
-re.compile(
+# Create a regex for phone numbers
+phoneRegex = re.compile(
     r"""
+# 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
 
-
-
-
+(\d\d\d)|(\(\d\d\d\))?   # area code(optional)
+(\s|-)                   # separator
+\d\d\d                   # first 3 digits
+-                        # separator
+\d\d\d\d                 # last 4 digits
+(((ext(\.)?\s)|x)        # extension word-part(optional)
+(\d{2,5}))?              # extension number-part(optional)
 """,
     re.VERBOSE,
 )
